da26a1ba36fe9ef9bfaccae025b3319e960199d2
Ry Lowry <ry.lowry@buckle.com>
2019-01-15 12:58:51 -0600

    Added some alternatives to the map problem.

--- src/main/java/com/cybr406/basics/BasicsController.java
+++ src/main/java/com/cybr406/basics/BasicsController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestController
@@ -38,9 +41,27 @@ public class BasicsController {
 
     @GetMapping("/map")
     public String map(@RequestParam MultiValueMap<String, String> params) {
+        // Possibility 1 - steams api
         return params.entrySet().stream()
-                .map(entry -> entry.getKey() + " values = " + String.join(", ", entry.getValue()))
+                .map(entry -> String.format("%s values = %s", entry.getKey(), String.join(", ", entry.getValue())))
                 .collect(Collectors.joining("\n"));
+        
+//        // Possibility 2 - For loop & String.join
+//        List<String> parts = new ArrayList<>();
+//        for (Map.Entry<String, List<String>> entry : params.entrySet()) {
+//            parts.add(entry.getKey() + " values = " + String.join(", ", entry.getValue()));
+//        }
+//        return String.join("\n", parts);
+        
+//        // Possibility 3 - For loop, string concatenation.
+//        // The trouble here is its hard to know when you are on the last entry.
+//        String output = "";
+//        for (Map.Entry<String, List<String>> entry : params.entrySet()) {
+//            output += entry.getKey() + " values = " + String.join(", ", entry.getValue()) + "\n";
+//        }
+//        // Manually remove the last newline
+//        String result = output.trim();
+//        return result;
     }
     
     @PostMapping("/register")
